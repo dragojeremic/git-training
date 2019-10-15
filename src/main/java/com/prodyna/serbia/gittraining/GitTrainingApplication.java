acdf5edaf8ee60691ef3a292d3706d9c94a3fc12
Drago Jeremic <drago.jeremic@prodyna.com>
2019-10-15 21:43:14 +0200

    initial commit

---
+++ src/main/java/com/prodyna/serbia/gittraining/GitTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.prodyna.serbia.gittraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTrainingApplication.class, args);
+	}
+
+}
